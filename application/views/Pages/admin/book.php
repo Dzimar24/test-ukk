a629498b51bf8dda1c9206f5b8b79186d690ff2e
Dzimar24 <ramadzimar@gmail.com>
2024-08-21 13:36:34 +0700

    Mit 4

--- application/views/Pages/admin/book.php
+++ application/views/Pages/admin/book.php
@@ -26,85 +26,83 @@
 				</div>
 				<div class="card-body">
 					<?= form_open_multipart('Admin/Book/Add') ?>
-					<div class="row">
-						<!-- Input for book cover image -->
-						<div class="col-md-3">
-							<label for="bookCover">Cover Book</label>
-							<div class="form-group">
-								<input type="file" name="cover" class="image-preview-filepond" id="bookCover" required>
-							</div>
-						</div>
-
-						<!-- Input fields for book details -->
-						<div class="col-md-9">
-							<div class="row">
-								<!-- Title and Category -->
-								<div class="col-md-9">
-									<div class="form-group">
-										<label for="bookTitle">Title:</label>
-										<input type="text" class="form-control" name="title" placeholder="Enter Book Title" id="bookTitle" required>
-									</div>
-								</div>
-								<div class="col-md-3">
-									<div class="form-group">
-										<label for="bookCategory">Category Book:</label>
-										<select name="categoryBook" class="form-control" id="categoryBook">
-											<option selected>-- Category Book --</option>
-											<?php foreach ($viewDataCategory as $key => $vdc): ?>
-												<option value="<?= $vdc['KategoriID'] ?>"><?= $vdc['NamaKategori'] ?></option>
-											<?php endforeach; ?>
-										</select>
-									</div>
+						<div class="row">
+							<!-- Input for book cover image -->
+							<div class="col-md-3">
+								<label for="bookCover">Cover Book</label>
+								<div class="form-group">
+									<input type="file" name="cover" class="image-preview-filepond" id="bookCover" required>
 								</div>
 							</div>
 
-							<div class="row">
-								<!-- Author -->
-								<div class="col-md-4">
-									<div class="form-group mt-2">
-										<label for="bookAuthor">Author:</label>
-										<input type="text" class="form-control" name="author" placeholder="Enter Book Author"
-											id="bookAuthor" required>
+							<!-- Input fields for book details -->
+							<div class="col-md-9">
+								<div class="row">
+									<!-- Title and Category -->
+									<div class="col-md-9">
+										<div class="form-group">
+											<label for="bookTitle">Title:</label>
+											<input type="text" class="form-control" name="title" placeholder="Enter Book Title" id="bookTitle" required>
+										</div>
+									</div>
+									<div class="col-md-3">
+										<div class="form-group">
+											<label for="bookCategory">Category Book:</label>
+											<select name="categoryBook" class="form-control" id="categoryBook">
+												<option selected>-- Category Book --</option>
+												<?php foreach ($viewDataCategory as $key => $vdc): ?>
+													<option value="<?= $vdc['KategoriID'] ?>"><?= $vdc['NamaKategori'] ?></option>
+												<?php endforeach; ?>
+											</select>
+										</div>
 									</div>
 								</div>
-								<!-- Publisher -->
-								<div class="col-md-4">
-									<div class="form-group mt-2">
-										<label for="bookPublisher">Publisher:</label>
-										<input type="text" class="form-control" name="publisher"
-											placeholder="Enter Book Publisher" id="bookPublisher" required>
+								<div class="row">
+									<!-- Author -->
+									<div class="col-md-4">
+										<div class="form-group mt-2">
+											<label for="bookAuthor">Author:</label>
+											<input type="text" class="form-control" name="author" placeholder="Enter Book Author" id="bookAuthor" required>
+										</div>
+									</div>
+									<!-- Publisher -->
+									<div class="col-md-4">
+										<div class="form-group mt-2">
+											<label for="bookPublisher">Publisher:</label>
+											<input type="text" class="form-control" name="publisher"
+												placeholder="Enter Book Publisher" id="bookPublisher" required>
+										</div>
+									</div>
+									<!-- Publication Year -->
+									<div class="col-md-4">
+										<div class="form-group mt-2">
+											<label for="publicationYear">Publication Year:</label>
+											<input type="date" class="form-control" name="publication_year" id="publicationYear"
+												required>
+										</div>
 									</div>
 								</div>
-								<!-- Publication Year -->
-								<div class="col-md-4">
-									<div class="form-group mt-2">
-										<label for="publicationYear">Publication Year:</label>
-										<input type="date" class="form-control" name="publication_year" id="publicationYear"
-											required>
-									</div>
+								<!-- Book Description -->
+								<div class="form-group mt-3">
+									<label for="default">Book Description:</label>
+									<textarea class="form-control" placeholder="Enter Book Description" name="description"
+										id="default" required></textarea>
 								</div>
-							</div>
-							<!-- Book Description -->
-							<div class="form-group mt-3">
-								<label for="default">Book Description:</label>
-								<textarea class="form-control" placeholder="Enter Book Description" name="description"
-									id="default" required></textarea>
 							</div>
 						</div>
-					</div>
-					<!-- Style Button Save Book -->
-					<style>
-						.button-custom {
-							display: flex;
-							justify-content: flex-end;
-							margin-top: 20px;
-							margin-right: 20px;
-						}
-					</style>
-					<!-- Save Button -->
-					<div class="button-custom">
-						<button type="submit" class="btn btn-primary">Save Book</button>
-					</div>
+						<!-- Style Button Save Book -->
+						<style>
+							.button-custom {
+								display: flex;
+								justify-content: flex-end;
+								margin-top: 20px;
+								margin-right: 20px;
+							}
+						</style>
+						<!-- Save Button -->
+						<div class="button-custom">
+							<button type="submit" class="btn btn-primary">Save Book</button>
+						</div>
 					<?= form_close(); ?>
 				</div>
 			</div>
