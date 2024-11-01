89fa66e9d74d4dad3cd351a91ab8554167ee4ddb
Dzimar24 <ramadzimar@gmail.com>
2024-11-01 20:39:55 +0700

    fix: Bug & feat: fullPage

--- application/views/Pages/public/fullPageBook.php
+++ application/views/Pages/public/fullPageBook.php
@@ -24,17 +24,19 @@
 <div class="container my-5">
    <h1 class="custom-title">Full Page</h1>
    <div class="row">
-      <div class="col-md-5">
+      <div class="col-md-4">
          <div class="custom-img-container">
             <img src="<?= base_url('/assets/uploads/coverBook/1724420818_cover.jpg') ?>" alt="Image Error" class="custom-img">
          </div>
       </div>
-      <div class="col-md-7">
-         <p class="text-content">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex non doloremque fugit?
-            Hic, veritatis quas praesentium saepe corporis quia quibusdam facere deleniti
-            aspernatur exercitationem aperiam accusantium eos odit aliquam quidem.
-         </p>
+      <div class="col-md-8">
+         <div class="card-body">
+            <p>
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex non doloremque fugit?
+               Hic, veritatis quas praesentium saepe corporis quia quibusdam facere deleniti
+               aspernatur exercitationem aperiam accusantium eos odit aliquam quidem.
+            </p>
+         </div>
       </div>
    </div>
 </div>
